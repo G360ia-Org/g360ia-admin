cc5bf5e90826a6dd12db1307f9df4f48a64c3150
PAblo <agencianlmd@gmail.com>
2026-03-20 21:23:33 -0300

    Update route.js

--- app/api/equipos/[id]/route.js
+++ app/api/equipos/[id]/route.js
@@ -1,7 +1,7 @@
 export const dynamic = "force-dynamic";
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../../auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
 import db from "../../../../lib/db";
 
 export async function GET(request, { params }) {
